340713d865e86e2680237bf297e8c22d016361f4
ShuttingDownExe <rishithkumar830@gmail.com>
2022-10-14 11:55:19 +0530

    Error Fix: IndexError: Layer [ARP] not found : sniff_helper.py

--- Helpers/sniff_helper.py
+++ Helpers/sniff_helper.py
@@ -1,5 +1,6 @@
 from scapy.layers.inet import IP
 from scapy.layers.http import HTTPRequest
+from scapy.layers.l2 import ARP
 from scapy.utils import wrpcap
 
 from Helpers.http_helper import process_HTTP_packet
@@ -15,7 +16,8 @@ class sniffer:
 def sniffer_func(pkt):
     wrpcap('PCAP_LOG.pcap', pkt, append=True)
 
-    arp_guard.spoof_guard(pkt)
+    if ARP in pkt:
+        arp_guard.spoof_guard(pkt)
 
     if IP in pkt:
         process_IP_packet(pkt)
