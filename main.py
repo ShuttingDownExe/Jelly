abfe13997e95fad185b9c3f409577fa142d570c1
ShuttingDownExe <rishithkumar830@gmail.com>
2022-10-23 12:34:52 +0530

    Code Update:- main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python3
 
 from scapy.all import *
-from Helpers.sniff_helper import sniffer_func, sniffer
+from Helpers.sniff_helper import sniffer_helper
 
 IP_blocklist = "firehol_level1.netset"
 
 
 if __name__ == '__main__':
-    sniff_obj = sniffer()
-    sniff(prn=sniffer_func)
+    snh = sniffer_helper()
+    sniff(prn=snh.sniffer_func)
