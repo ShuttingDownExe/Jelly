076555fc7066cdbba00092bd92f046288de9c2b8
ShuttingDownExe <51394715+ShuttingDownExe@users.noreply.github.com>
2022-12-14 06:10:05 +0000

    Code Update: parallel_helper

--- Helpers/parallel_helper.py
+++ Helpers/parallel_helper.py
@@ -4,12 +4,17 @@ from Helpers.sniff_helper import sniffer_helper as snh
 
 process_list = []
 
+def clean():
+    for p in process_list:
+        if not p.is_alive():
+            process_list.pop(process_list.index(p))
+
+
 
 def chain(pkt):
     if len(process_list) < 20:
         process_list.append(multiprocessing.Process(target=snh.sniffer_func, args=(pkt,)))
         process_list[len(process_list) - 1].start()
     else:
-        process_list[0].join()
-        process_list.remove(process_list[0])
+        clean()
         chain(pkt)
